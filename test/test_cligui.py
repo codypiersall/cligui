547a1ebbab97298529eb1463b1976a84e62ab6ee
Cody Piersall <cody.piersall@gmail.com>
2015-02-02 23:40:57 -0600

    Make parser do something useless (instead of nothing).

--- test/test_cligui.py
+++ test/test_cligui.py
@@ -4,13 +4,20 @@ import argparse
 
 
 def get_parser():
-    p = argparse.ArgumentParser(description='test parser')
+    p = argparse.ArgumentParser(description='Add a couple numbers together; write results to stdout.')
 
-    p.add_argument('infile',
-                   help='input file')
+    p.add_argument('addend1',
+                   help='First number to add',
+                   type=float)
 
-    p.add_argument('outfile',
-                   help='output file')
+    p.add_argument('addend2',
+                   help='Second number to add',
+                   type=float)
+
+    p.add_argument('-m', '--multiplier',
+                   help='If you want to multiply the answer...',
+                   type=float,
+                   default=1)
 
     p.add_argument('-v', '--verbose',
                    help='Increase verbosity',
@@ -23,6 +30,3 @@ def get_parser():
 def test_cligui():
     p = get_parser()
     gui = CliGui(p)
-
-
-
